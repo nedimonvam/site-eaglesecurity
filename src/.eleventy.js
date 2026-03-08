ce0fe7f5e60d34e774ef14039f8278ab2613fe13
nedimonvam <nedimonvam@gmail.com>
2026-03-08 11:38:46 +0100

    Updates

---
+++ src/.eleventy.js
@@ -0,0 +1,23 @@
+module.exports = function (eleventyConfig) {
+  const pathPrefix = process.env.GITHUB_PAGES ? "/site-eaglesecurity" : "";
+  eleventyConfig.addGlobalData("pathPrefix", pathPrefix);
+
+  eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPassthroughCopy("content/**/*.png");
+  eleventyConfig.addPassthroughCopy("content/**/*.jpg");
+  eleventyConfig.addPassthroughCopy("content/**/*.webp");
+  // Copy logo from project root input/ if present (e.g. input/logo.png -> assets/img/logo.png)
+  eleventyConfig.addPassthroughCopy({ "../input/logo.png": "assets/img/logo.png" });
+  eleventyConfig.addPassthroughCopy({ "../input/logo.jpg": "assets/img/logo.jpg" });
+  eleventyConfig.addPassthroughCopy({ "../input/logo.webp": "assets/img/logo.webp" });
+
+  return {
+    dir: {
+      input: "content",
+      includes: "../_includes",
+      data: "../_data",
+      output: "_site",
+    },
+    pathPrefix: pathPrefix ? pathPrefix + "/" : "",
+  };
+};
